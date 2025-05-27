11558aa3ef4925e45834df04aaba377e63612b0d
NielsKok <NielsKok01@rabobank.com>
2025-05-27 10:26:19 +0200

    Chapter3

--- course/3-functions/exercises/4-pass_by_value/code.go
+++ course/3-functions/exercises/4-pass_by_value/code.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	sendsSoFar := 430
 	const sendsToAdd = 25
-	incrementSends(sendsSoFar, sendsToAdd)
-	fmt.Println("you've sent", sendsSoFar, "messages")
+	fmt.Println("you've sent", incrementSends(sendsSoFar, sendsToAdd), "messages")
 }
 
 func incrementSends(sendsSoFar, sendsToAdd int) int {
 	sendsSoFar = sendsSoFar + sendsToAdd
+	return sendsSoFar
 }
